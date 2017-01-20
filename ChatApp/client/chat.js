dede1975843d8af8b66ba5ead87e75e6c780638c
Mario <Mario>
2017-01-20 00:26:45 -0500

    added basic app functionality

--- ChatApp/client/chat.js
+++ ChatApp/client/chat.js
@@ -1,2 +1,43 @@
 
 
+Messages = new Mongo.Collection("msgs");
+
+Meteor.methods({
+  sendMessage: function (messageText) {
+    /* add authentication here */
+
+    Messages.insert({
+      messageText: messageText,
+      createdAt: new Date(),
+      username: "anonymous"
+    });
+  }
+});
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("messages", function () {
+    return Messages.find({}, {sort: {createdAt: -1}, limit: 5});
+  });
+}
+
+/* scrolling code */
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("messages");
+
+  /* helper code */
+  Template.body.helpers({
+    recentMessages: function () {
+      return Messages.find({}, {sort: {createdAt: 1}});
+    },
+    /* unread message helper */
+  });
+
+  /*chat window scrolling*/
+
+  /*events*/
+
+  /*account config*/
+}
