45142542329fbe0a7af418a7d23c387f42971d83
Mario <Mario>
2017-01-21 14:02:32 -0500

    fixed message bug, messages come up on screen now

--- ChatApp/client/chat.js
+++ ChatApp/client/chat.js
@@ -1,61 +1,69 @@
-Messages = new Mongo.Collection("msgs");
 
-Meteor.methods({
-  sendMessage: function (message) {
-    if (! Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
 
-    Messages.insert({
-      messageText: message,
-      createdAt: new Date(),
-      username: Meteor.user().username  // <-add real username
-    });
+
+var autoScrollingIsActive = false;
+/* reactive var here */
+scrollToBottom = function scrollToBottom (duration) {
+  var messageWindow = $(".message-window");
+  var scrollHeight = messageWindow.prop("scrollHeight");
+  messageWindow.stop().animate({scrollTop: scrollHeight}, duration || 0);
+};
+
+// This code only runs on the client
+Meteor.subscribe("messages", {
+  onReady: function () {
+      scrollToBottom();
+      autoScrollingIsActive = true;
   }
 });
-
-if (Meteor.isServer) {
-  // This code only runs on the server
-  Meteor.publish("messages", function () {
-    return Messages.find();
-  });
-}
-
-/* scrolling code */
-
-if (Meteor.isClient) {
-  // This code only runs on the client
-  Meteor.subscribe("messages");
 
   /* helper code */
   Template.body.helpers({
     recentMessages: function () {
       return Messages.find({}, {sort: {createdAt: 1}});
-    },
+    }
     /* unread message helper */
-  });
+  })
 
   /*chat window scrolling*/
+   Template.message.onRendered(function () {
+    if (autoScrollingIsActive) {
+      scrollToBottom(250);
+    } 
+  }); 
+
+   Template.message.onRendered(function () {
+    if (autoScrollingIsActive) {
+      scrollToBottom(250);
+    } 
+  });
 
   /*events*/
   Template.body.events({
-    "submit .new-message": function (event) {
-      var text = event.target.text.value;
+    "submit .new-message": function(event) {
+      var messageText = event.target.text.value;
 
-      Meteor.call("sendMessage", text);
+      Meteor.call("sendMessage", messageText);
+      scrollToBottom(250); // <--add this line
 
       event.target.text.value = "";
       event.preventDefault();
     },
 
     /* scroll event */
+    "scroll .message-window": function () {
+      var howClose = 80;  // # pixels leeway to be considered "at Bottom"
+      var messageWindow = $(".message-window");
+      var scrollHeight = messageWindow.prop("scrollHeight");
+      var scrollBottom = messageWindow.prop("scrollTop") + messageWindow.height();
+      var atBottom = scrollBottom > (scrollHeight - howClose);
+      autoScrollingIsActive = atBottom ? true : false;
+    },
 
   });
-
-   /*account config*/ 
+  
+  /*account config*/ 
    // Keep this if you want it to ask for username instead of email
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
-
-}
--- ChatApp/server/main.js
+++ ChatApp/server/main.js
@@ -1,6 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  /* scrolling code */
+  Meteor.publish("messages", function () {
+    return Messages.find();
+  });
+});
+
+Meteor.methods({
+  sendMessage: function (message) {
+    // if (! Meteor.userId()) {
+    //   throw new Meteor.Error("not-authorized");
+    // }
+
+    Messages.insert({
+      messageText: message
+      // createdAt: new Date(),
+      // username: Meteor.user().username  // <-add real username
+    });
+  }
 });
 
