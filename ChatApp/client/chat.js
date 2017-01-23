ba409f578dab88297cee681cc3bca15b9325d0fd
Mario <Mario>
2017-01-22 23:42:50 -0500

    added unread message notification

--- ChatApp/client/chat.js
+++ ChatApp/client/chat.js
@@ -1,15 +1,17 @@
-
+import { Meteor } from 'meteor/meteor';
 
 
 var autoScrollingIsActive = false;
-/* reactive var here */
-scrollToBottom = function scrollToBottom (duration) {
+// reactive var
+thereAreUnreadMessages = new ReactiveVar(false);
+
+scrollToBottom = function (duration) {
   var messageWindow = $(".message-window");
   var scrollHeight = messageWindow.prop("scrollHeight");
   messageWindow.stop().animate({scrollTop: scrollHeight}, duration || 0);
 };
 
-// This code only runs on the client
+// subscribing to messages publication
 Meteor.subscribe("messages", {
   onReady: function () {
       scrollToBottom();
@@ -21,22 +23,25 @@ Meteor.subscribe("messages", {
   Template.body.helpers({
     recentMessages: function () {
       return Messages.find({}, {sort: {createdAt: 1}});
-    }
+    },
     /* unread message helper */
+    thereAreUnreadMessages: function () {
+      return thereAreUnreadMessages.get();
+    }
+
   })
 
   /*chat window scrolling*/
-   Template.message.onRendered(function () {
+  Template.message.onRendered(function () {
     if (autoScrollingIsActive) {
       scrollToBottom(250);
-    } 
-  }); 
-
-   Template.message.onRendered(function () {
-    if (autoScrollingIsActive) {
-      scrollToBottom(250);
-    } 
+    } else {
+      if (Meteor.user() && this.data.username !== Meteor.user().username) {
+        thereAreUnreadMessages.set(true);
+      }
+    }
   });
+
 
   /*events*/
   Template.body.events({
@@ -44,7 +49,7 @@ Meteor.subscribe("messages", {
       var messageText = event.target.text.value;
 
       Meteor.call("sendMessage", messageText);
-      scrollToBottom(250); // <--add this line
+      scrollToBottom(250);
 
       event.target.text.value = "";
       event.preventDefault();
@@ -58,7 +63,15 @@ Meteor.subscribe("messages", {
       var scrollBottom = messageWindow.prop("scrollTop") + messageWindow.height();
       var atBottom = scrollBottom > (scrollHeight - howClose);
       autoScrollingIsActive = atBottom ? true : false;
+      if (atBottom) {        
+        thereAreUnreadMessages.set(false);
+      }
     },
+
+    "click .more-messages": function () {
+      scrollToBottom(500);
+      thereAreUnreadMessages.set(false);
+    }
 
   });
   
--- ChatApp/server/main.js
+++ ChatApp/server/main.js
@@ -9,14 +9,14 @@ Meteor.startup(() => {
 
 Meteor.methods({
   sendMessage: function (message) {
-    // if (! Meteor.userId()) {
-    //   throw new Meteor.Error("not-authorized");
-    // }
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
 
     Messages.insert({
-      messageText: message
-      // createdAt: new Date(),
-      // username: Meteor.user().username  // <-add real username
+      messageText: message,
+      createdAt: new Date(),
+      username: Meteor.user().username  // <-adds real username
     });
   }
 });
